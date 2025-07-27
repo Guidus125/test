b6be686b2abffb6619642846fb091c027c663e73
Guidus125 <82458641+Guidus125@users.noreply.github.com>
2025-07-27 01:00:38 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react‑router‑dom";  // ← add this import
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/test">    {/* ← wrap App in this */}
+    <App />
+  </BrowserRouter>
+);
